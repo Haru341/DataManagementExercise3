304fd21640e63c5b34ce3a9a4b0e266012f56bdb
Haru <mura31454gmail.com>
2025-07-03 23:36:07 +0900

    Won/lost message modified

--- diceGame.c
+++ diceGame.c
@@ -3,28 +3,28 @@
 #include <time.h>
 
 int main(){
-    srand(time(NULL));
-
-    int d1 = rand() % 6 + 1;
-    int d2 = rand() % 6 + 1;
-
-    printf("Rolling dice...\n");
-    printf("Die 1: %d\n", d1);
-    printf("Die 2: %d\n", d2);
-    printf("Total value: %d\n", d1 + d2);
-
+    int d1,d2,total;
     char name[100];
-    printf("What is your name?\n");
+    
+    srand(time(NULL));
+    
+    printf("What is your name?\n>");
     scanf("%99s", name);
-    printf("Hello, %s!", name);
+    printf("Hello, %s!\n", name);
 
-    int d1 = rand() % 6 + 1;
-    int d2 = rand() % 6 + 1;
-
+    d1 = rand() % 6 + 1;
+    d2 = rand() % 6 + 1;
+    total = d1 + d2;
     printf("Rolling dice...\n");
     printf("Die 1: %d\n", d1);
     printf("Die 2: %d\n", d2);
-    printf("Total value: %d\n", d1 + d2);
+    printf("Total value: %d\n", total);
+
+    if(total > 7){
+        printf("%s won!\n", name);
+    } else {
+        printf("%s lost!\n", name);
+    }
 
     return 0;
 }    
